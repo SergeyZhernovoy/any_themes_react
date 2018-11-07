9c9c86a4336016928a93ffecc5fa48b4367dfdd0
= <szhernov@gmail.com>
2018-11-07 18:19:33 +0300

    update 07/11/18

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+function formatName(user) {
+  return user.name + ' ' + user.surname;
+}
+
 class App extends Component {
   render() {
-    var helloWorld = 'Добро пожаловать в Путь изучения React';
+    const helloWorld = 'Добро пожаловать в Путь изучения React';
+    var user = {name: 'Sergey', surname : 'Zhernovoy', age : 40};
     return (
       <div className="App">
         <h2>{helloWorld}</h2>
+        <h3>Hello , {formatName(user)}</h3>
+        <h2>Сейчас {new Date().toLocaleTimeString()}</h2>
       </div>
     );
   }
