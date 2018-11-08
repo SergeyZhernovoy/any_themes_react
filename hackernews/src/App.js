1ecaeb03ecb3fc04b9a81faee6315b86aec675ee
= <szhernov@gmail.com>
2018-11-08 14:37:27 +0300

    add func

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -2,41 +2,78 @@ import React, { Component } from 'react';
 import './App.css';
 
 const list = [
-{
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0
-},
-{
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1
-}
-
-
-
-
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
 ];
 
-function formatName(user) {
-  return user.name + ' ' + user.surname;
+const user = {
+  name: 'Sergey Zhernovoy',
+  age: 40
 }
-
+/**
+ *
+ *
+ * @class App
+ * @extends {Component}
+ */
 class App extends Component {
-  render() {
-    const helloWorld = 'Добро пожаловать в Путь изучения React';
-    var user = {name: 'Sergey', surname : 'Zhernovoy', age : 40};
+/**
+ *Creates an instance of App.
+ * @param {*} props
+ * @memberof App
+ */
+constructor(props) {
+    super(props);
+    this.state = {
+      list,
+      user
+    }
+    this.onDismiss = this.onDismiss.bind(this);
+    this.clickMe = this.clickMe.bind(this);
+  }
+
+  clickMe() {
+    console.log(this);
+  }
+
+
+/**
+ *
+ *
+ * @param {*} id
+ * @memberof App
+ */
+onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updateList = this.state.list.filter(isNotId);
+    this.setState({list: updateList});
+  }
+
+  /**
+   *
+   *
+   * @returns
+   * @memberof App
+   */
+render() {
     return (
       <div className="App">
        {
-         list.map(function (item) {
-           return (
+         this.state.list.map(item =>
            <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title} </a>
@@ -44,9 +81,26 @@ class App extends Component {
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <p><span>{this.state.user.name}</span></p>
+              <span>
+                <button onClick={() => this.onDismiss(item.objectID)}
+                type = "button"
+                >
+                  Отбросить
+                </button>
+              </span>
+              <span>
+                <button onClick={this.clickMe} type="button">
+                  Кликни на меня.
+                </button>
+              </span>
+              <span>
+                <button onClick={()=>console.log(item.objectID)} type="button">
+                  Смотри логи .
+                </button>
+              </span>
            </div>
-           );
-         })
+          )
        }
       </div>
     );
