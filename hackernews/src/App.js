3bc8e4eb48ed9bf1a4a5878bedbc5e5cb6b5362a
Sergey Zhernovoy <szhernov@gmail.com>
2018-11-08 08:58:14 +0300

    add list map

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+{
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0
+},
+{
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1
+}
+
+
+
+
+];
 
 function formatName(user) {
   return user.name + ' ' + user.surname;
@@ -12,9 +34,20 @@ class App extends Component {
     var user = {name: 'Sergey', surname : 'Zhernovoy', age : 40};
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
-        <h3>Hello , {formatName(user)}</h3>
-        <h2>Сейчас {new Date().toLocaleTimeString()}</h2>
+       {
+         list.map(function (item) {
+           return (
+           <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title} </a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+           </div>
+           );
+         })
+       }
       </div>
     );
   }
