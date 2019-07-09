f3d5e949c943872898fe2077c74ceca3e8d0b9f0
Sergey Zhernovoy <szhernov@gmail.com>
2019-07-09 18:15:32 +0300

    update 090719

--- todo/src/App.js
+++ todo/src/App.js
@@ -7,19 +7,19 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "Sergey"
-    }
-  }
-
-  changeStateData = () => {
-    this.setState({
-      userName: this.state.userName === "Sergey" ? "Bob" : "Sergey",
+      userName: "Sergey",
       todoItems: [{action: "Buy Flower", done: false},
         {action: "Get shoes", done: false},
         {action: "Collect tickets", done: true},
         {action: "Call Joe", done: false}],
       newItemText: ""
-    })
+    }
+  }
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Sergey" ? "Bob" : "Sergey"
+    });
   }
 
   updateNewTextValue = (event) => {
@@ -31,12 +31,30 @@ class App extends Component {
   createNewTodo = () => {
     if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
       this.setState({
-        tooItems: [...this.state.todoItems, {action: this.state.newItemText, done: false}],
+        todoItems: [...this.state.todoItems, {action: this.state.newItemText, done: false}],
         newItemText: ""
       });
     }
   }
 
+  toggleTodo = (todo) => this.setState(
+      {
+        todoItems: this.state.todoItems.map(item => item.action === todo.action
+            ? {...item, done: !item.done} : item)
+  });
+
+  todoTableRows = () => this.state.todoItems.map(item =>
+        <tr key={item.action}>
+          <td>{item.action}</td>
+          <td>
+            <input
+                type="checkbox"
+                checked={item.done}
+                onChange={() => this.toggleTodo(item)}
+            />
+          </td>
+        </tr>
+  );
 
   render() {
     return (
@@ -57,6 +75,12 @@ class App extends Component {
         <button className="btn btn-success m-2" onClick={this.changeStateData}>
           Change name
         </button>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr><th>Description</th><th>Done</th></tr>
+          </thead>
+          <tbody>{this.todoTableRows()}</tbody>
+        </table>
       </div>
     );
   }
